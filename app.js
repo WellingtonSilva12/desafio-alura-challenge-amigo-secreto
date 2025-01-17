e252a936139aef26752483bac6a0846eb67f9708
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2025-01-17 08:39:52 -0300

    Implement a function to add friends

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ let friendsList = []
 
 function addFriend() {
     const inputField = document.getElementById('friend')
+    const nameValidation = /^[a-zA-ZÀ-ÿ ]+$/;
 
-    if(inputField) {
-        friendsList.push(inputField)
+
+    if(inputField.value.trim() !== '' && nameValidation.test(inputField.value)) {
+        friendsList.push(inputField.value)
+
+        console.log('nome adicionado com sucesso!')
+        console.log(friendsList)
+
+        updateList()
     } else {
         alert('Por favor, insira um nome válido.')
     }
+}
+
+function updateList(){
+    const ul = document.getElementById('friendList')
+    ul.innerHTML = ''
+    friendsList.forEach(name => {
+        const li = document.createElement('li')
+        li.textContent = name
+        ul.appendChild(li)
+    })
 }
