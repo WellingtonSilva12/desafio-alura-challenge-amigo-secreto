57285f983f3097bfc2838a2e61e67cf752208d83
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2025-01-20 15:04:32 -0300

    Implementation of the function to add the name to an external server in node.js

--- app.js
+++ app.js
@@ -3,24 +3,53 @@ let friendsList = []
 
 
 function addFriend() {
-    const inputField = document.getElementById('friend')
+    const inputField = document.getElementById('friend');
     const nameValidation = /^[a-zA-ZÀ-ÿ ]+$/;
 
-
     if (inputField.value.trim() !== '' && nameValidation.test(inputField.value)) {
-        if (!friendsList.includes(inputField.value.trim())) {
-            friendsList.push(inputField.value.trim())
-            console.log('nome adicionado com sucesso!')
-            console.log(friendsList)
-            updateList()
-            inputField.value = ''
+        const friendName = inputField.value.trim();
+
+        if (!friendsList.includes(friendName)) {
+            addNameFriendServer(friendName)
+                .then(response => {
+                    if (response.error) {
+                        alert(response.error);
+                    } else {
+                        friendsList.push(friendName);
+                        console.log('Nome adicionado com sucesso!');
+                        updateList();
+                        inputField.value = '';
+                        // alert(response.message); 
+                    }
+                })
+                .catch(error => {
+                    console.error('Erro ao adicionar amigo no servidor:', error);
+                    alert('Erro ao se comunicar com o servidor.');
+                });
         } else {
-            alert('Este nome já foi adicionado.')
+            alert('Este nome já foi adicionado.');
         }
     } else {
-        alert('Por favor, insira um nome válido.')
+        alert('Por favor, insira um nome válido.');
     }
 }
+
+async function addNameFriendServer(name) {
+    const response = await fetch('http://localhost:3000/add-friend', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name }), 
+    });
+    if (!response.ok) {
+        return response.json().then(error => {
+            throw new Error(error.error || 'Erro desconhecido no servidor');
+        });
+    }
+    return await response.json();
+}
+
 
 document.getElementById('friend').addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
